49eb1fa48ef6d82be49ffb04b1c95221277f48c4
wuxiaogang <940374606@qq.com>
2020-05-11 22:18:47 +0800

    rml-docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+    }
+};
