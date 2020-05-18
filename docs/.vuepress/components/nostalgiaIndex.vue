a86fc0f99690d91c8a6e740bcd30884f1d8df371
wuxiaogang <940374606@qq.com>
2020-05-18 21:05:54 +0800

    fix

--- docs/.vuepress/components/nostalgiaIndex.vue
+++ docs/.vuepress/components/nostalgiaIndex.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-        <img src="/rml-docs/assets/img/nostalgia.jpg" alt="">
-    </div>
+	<div>
+		<img src="/rml-docs/assets/img/nostalgia.jpg" alt="">
+	</div>
 </template>
 <script>
-    export default {
-    	name: 'nostalgiaIndex',
-        mounted(){
-    		document.querySelector('.page').style.width = '100%';
-        }
-    }
+	export default {
+		name: 'nostalgiaIndex',
+		mounted() {
+			if(document.querySelector('.page')){
+				document.querySelector('.page').classList.add('width_100');
+			}
+		},
+		beforeDestroy() {
+			if(document.querySelector('.page')){
+				document.querySelector('.page').classList.remove('width_100');
+			}
+		}
+	}
 </script>
 <style lang="css" scoped>
 </style>
