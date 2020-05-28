c9534669368cbdb23d867d04fa3276fadf54f323
wuxiaogang <940374606@qq.com>
2020-05-28 22:26:19 +0800

    rml-docs：fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-moment.locale('zh-cn');
+// moment.locale('zh-cn');
 
 module.exports = {
 	title: '云中白鹤',
@@ -11,20 +11,16 @@ module.exports = {
 	},
 	plugins: {
 		'@vuepress/back-to-top': true,
-		'@vuepress/last-updated': (timestamp) => moment(timestamp).format('llll'),
+		// '@vuepress/last-updated': (timestamp) => moment(timestamp).format('llll'),
 		'@vuepress/active-header-links': true,
 	},
 	themeConfig: {
-		lastUpdated: '更新时间',
+		// lastUpdated: '更新时间',
 		search: false, // 禁用默认的搜索框
-		// 图标
-		// logo: '/assets/img/logo.png',
 		// 导航栏
 		nav: [
 			{text: '首页', link: '/'},
 			{text: '笔记', link: '/note'},
-			// {text: '乡愁', link: '/nostalgia'},
-			// {text: '关于我', link: '/about'},
 		],
 		// 侧边栏
 		sidebar: [
@@ -42,36 +38,21 @@ module.exports = {
 				path: '/vue/',
 				collapsable: false,
 				sidebarDepth: 1,
-				children: [
-					'/vue/code'
-				]
+				children: ['/vue/code',]
             },
             {
                 title: 'node',
 				path: '/node/',
 				collapsable: false,
 				sidebarDepth: 1,
-				children: [
-					'/node/express'
-				]
+				children: ['/node/express',]
             },
-			// {
-			// 	title: '弹框',
-			// 	path: '/layer/',
-			// 	collapsable: false,
-			// 	sidebarDepth: 1,
-			// 	children: [
-			// 		'/layer/code'
-			// 	]
-			// },
 			{
 				title: 'Electron',
 				path: '/electron/',
 				collapsable: false,
 				sidebarDepth: 1,
-				children: [
-					'/electron/code'
-				]
+				children: ['/electron/code',]
 			},
 		],
 		// 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
