ca425971c48a93d9315207051a8f9e22e2b2ff2a
吴小刚 <940374606@qq.com>
2020-05-31 17:58:53 +0800

    rml-docs：fix

--- docs/.vuepress/config/navConfig.js
+++ docs/.vuepress/config/navConfig.js
@@ -1,6 +1,7 @@
 module.exports = [
 	{text: '首页', link: '/'},
 	{text: '笔记', link: '/note'},
+	{text: '悅听', link: '/blog.github.io/music/index.html', target: '_blank'},
 	{
 		text: '乡愁',
 		items: [
